8fbf9b6b9146ea8dfb3c2fef2c8de4d50370b317
arthas <arthastheking113@gmail.com>
2023-05-23 20:47:38 -0400

    update seo

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const title = "Duy Lan Le - Software Engineer Portfolio";
+  const description = "I'm Duy Lan Le and a Software Engineer";
+  const image = "/images/about/avatar.jpg";
   return (
     <Html lang="en">
       <Head>
@@ -8,12 +11,24 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto+Slab:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        <meta
-          name="description"
-          content="Duy Lan Le - Portfolio"
-        />
-        <meta content="duy lan le portfolio" name="description" />
-        <meta content="duy lan le" name="keywords" />
+        <title>{title}</title>
+        <meta name="description" content={description}/>
+
+        <meta itemprop="name" content={title}/>
+        <meta itemprop="description" content={description}/>
+        <meta itemprop="image" content={image}/>
+
+        <meta property="og:url" content="https://duylanle.com"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content={title}/>
+        <meta property="og:description" content={description}/>
+        <meta property="og:image" content={image}/>
+
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content={title}/>
+        <meta name="twitter:description" content={description}/>
+        <meta name="twitter:image" content={image}/>
+        <meta content="duy lan le, software engineer, lan le, arthastheking113@gmail.com, lanle97work@gmail.com" name="keywords" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
